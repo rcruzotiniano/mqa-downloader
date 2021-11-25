0a664c9b8c1a4977eaa0cd14fddbe0f6679879a7
Richard Jorge Cruz Otiniano <rcruzotiniano@gmail.com>
2021-11-24 22:14:58 -0500

    Revert "TAG lyrics"
    This reverts commit c519ecb5d594eec4b170e2bb0c1790463c3843ce.

--- config/settings.py
+++ config/settings.py
@@ -71,8 +71,7 @@ TV_SECRET = "eVWBEkuL2FCjxgjOkR3yK0RYZEbcrMXRc2l8fU3ZCdE="
 # Web token for hidden web login (do not use!)
 WEB_TOKEN = "CzET4vdadNUFQ5JU"
 
-# path = "/data/data/com.termux/files/home/storage/music"
-path = "E:\MQA"
+path = "/data/data/com.termux/files/home/storage/music"
 
 PRESETS = {
 
@@ -91,8 +90,8 @@ PRESETS = {
         "convert_to_alac": False,
         "save_credits_txt": False,
         "embed_credits": True,
-        "save_lyrics_lrc": True,
-        "embed_lyrics": True,
+        "save_lyrics_lrc": False,
+        "embed_lyrics": False,
         "genre_language": "en-US",
         "artwork_size": 1200,
         "uncompressed_artwork": True,
@@ -105,7 +104,7 @@ PRESETS = {
 
     # This will download the highest available quality including MQA
     "best_available": {
-        "keep_cover_jpg": True,
+        "keep_cover_jpg": False,
         "embed_album_art": True,
         "save_album_json": False,
         "aggressive_remix_filtering": True,
@@ -118,11 +117,11 @@ PRESETS = {
         "album_format": "{albumartist} - {album}{quality}{explicit}",
         "video_folder_format": "{artist} - {title}{quality}",
         "video_file_format": "{title}",
-        "convert_to_alac": False,
+        "convert_to_alac": True,
         "save_credits_txt": False,
         "embed_credits": True,
-        "save_lyrics_lrc": True,
-        "embed_lyrics": True,
+        "save_lyrics_lrc": False,
+        "embed_lyrics": False,
         "genre_language": "en-US",
         "artwork_size": 1200,
         "uncompressed_artwork": True,
@@ -148,8 +147,8 @@ PRESETS = {
         "convert_to_alac": False,
         "save_credits_txt": False,
         "embed_credits": True,
-        "save_lyrics_lrc": True,
-        "embed_lyrics": True,
+        "save_lyrics_lrc": False,
+        "embed_lyrics": False,
         "genre_language": "en-US",
         "artwork_size": 1200,
         "uncompressed_artwork": False,
@@ -175,8 +174,8 @@ PRESETS = {
         "convert_to_alac": False,
         "save_credits_txt": False,
         "embed_credits": True,
-        "save_lyrics_lrc": True,
-        "embed_lyrics": True,
+        "save_lyrics_lrc": False,
+        "embed_lyrics": False,
         "genre_language": "en-US",
         "artwork_size": 1200,
         "uncompressed_artwork": True,
